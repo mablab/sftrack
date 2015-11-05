9db6c3537e576e9aa19a8ec5bfeb22de6799307b
Steph Locke <slocke@mango-solutions.com>
2015-11-05 20:16:59 +0000

    Less dir switching

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -10,13 +10,10 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git config --global user.name "stephs-travis"
 git config --global user.email "steph@travis.ci"
 
-cd out
-git init
-
-cd ../
 R CMD BATCH 'ghgenerate.R'
 
 cd out
+git init
 git add .
 git commit -m "deployed to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
