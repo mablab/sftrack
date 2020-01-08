cbfe7b3b24275672357481dfb287d94ff43eab33
birderboone <mttboone@gmail.com>
2020-01-08 16:17:17 -0700

    creating make_step and barebones class

--- R/sfstep.R
+++ R/sfstep.R
@@ -1,55 +1,40 @@
 library(sf)
 library(sftraj)
 df1 <- read.csv('/home/matt/Downloads/data-1576521725288.csv')
-df1 <- df1[df1$sensor_code=='CJ11',]
+#df1 <- df1[df1$sensor_code=='CJ11',]
 df2 <- st_as_sf(df1, coords=c('latitude','longitude'))
 burstz <- list(month = as.POSIXlt(df1$utc_date)$mon, height =as.numeric(df1$height>5))
-id = df1$sensor_code
-my_traj <- new_sftraj(df1, time =as.POSIXct(df1$acquisition_time), id = df1$sensor_code,
-   error = NA, coords = c('longitude','latitude','height'), tz = 'UTC',
-   burst = burstz)
-#Works to convert from sftraj
-# Try not to change
-df3 <- df2[is.na(as.numeric(st_intersects(df2$geometry,st_point(c(0,0))))),]
-x1 <- df3$geometry[1:(nrow(df3)-1)]
-x2 <- df3$geometry[2:nrow(df3)]
-df3 <- df3[2:(nrow(df3)),]
-df3$geom2 <- NA
-df3$geom2 <- st_sfc(mapply(function(x,y) st_linestring(rbind(x, y)), x1, x2, SIMPLIFY = F))
+# Make a step geometry
+# Must be ordered, does not descriminate!
+make_step_geom <- function(burst = data_sf$burst, geometry = data_sf$geometry){
+  # Need to check if time is ordered, if not throw an error
 
-plot(df3$geom2)
-burst[['id']] <- id
-burst_list <- do.call(function(...) mapply(list,...,SIMPLIFY=F), burst)
-##
+  burst = data_sf$burst
+  idz <- sapply(burst, function(x) x$id)
+  unique_idz <- levels(idz)[table(idz)>0]
 
-# Whats the bare minimum you need for the class though
+  step_geometry <- rep(NA,length(geometry))
+  for(i in unique_idz){
+    #  i <- unique_idz[1]
+    subz <- idz==i
 
-y = df1$latitude
-x = df1$longitude
-timez = as.POSIXct(df1$acquisition_time)
-idz = df1$sensor_code
+    #We cant actually inject a null point and have it convert to line string, so we have to deal with that later
 
-####
-# Note for tomorrow
-# Have to figure out how to order points before calculating
-# Particularly for multiple IDs
+    x1 <- geometry[subz][1:(sum(subz)-1)]
 
-df3$geometry <- st_sfc(mapply(function(x,y) st_linestring(rbind(x, y)), x1, x2, SIMPLIFY = F))
+    x2 <- geometry[subz][2:sum(subz)]
+    first_point <- min(which(subz))
+    #subz[first_point] <- FALSE
+    x3 <- mapply(function(x,y) { st_linestring(rbind(x, y)) }, x1, x2, SIMPLIFY = F)
 
+    step_geometry[subz] <- c(st_sfc(st_linestring(x = matrix(numeric(0), 0, 3), dim = "XYZ")),st_sfc(x3))
 
-sf::st_as_sf(
-  data.frame(
-    id = seq_len(nrow(data)),
-    data,
-    time_traj = new_time_tj(time,id=id,tz=tz),
-    burst = make_multi_burst(id=id, burst=burst),
-    error = error
-  ),
-  coords = coords,
-  dim = 'XYZ'
-)
-##############
-# Make class
+  }
+  return(st_sfc(step_geometry))
+}
+
+make_step_geom()
+# Whats the bare minimum you need for the class
 new_sfstep<-
   function(data = data.frame(),
     proj4 = NA,
@@ -57,27 +42,38 @@ new_sfstep<-
     id = NA,
     burst = NULL,
     error = NA,
-    coords = c('x','y'),
+    coords = c('x','y','z'),
     tz = NULL
-  ) {
-    burst[['id']] <- id
-    burst_list <- do.call(function(...) mapply(list,...,SIMPLIFY=F), burst)
-    
+  )  {
+    coords = c('longitude', 'latitude','height')
+
+    data = df1
+    time = as.POSIXct(df1$acquisition_time)
+    id = df1$sensor_code
+    error = error
+    tz = NULL
+
+    #convert to sf object
+    # tapply(timez, idz, duplicated)
+    # unique_q <- tapply(timez, idz, function(x)
+    #   any(duplicated(x)))
+
+    data_sf <- new_sftraj(data, time =time, id = id,
+      error = NA, coords = coords, tz = 'UTC',
+      burst = burstz)
+    torder <- order(time)
+    data_sf <- data_sf[torder,]
+    # Function to make the step geometry column
+
+    data_sf$step_geometry <- make_step_geom(burst = data_sf$burst, geometry = data_sf$geometry)
+
     structure(
-      sf::st_as_sf(
-        data.frame(
-          id = seq_len(nrow(data)),
-          data,
-          time_traj = new_time_tj(time,id=id,tz=tz),
-          burst = make_multi_burst(id=id, burst=burst),
-          error = error
-        ),
-        coords = coords,
-        dim = 'XYZ'
+      data_sf1 <- sf::st_sf(
+        data_sf,
+        geometry=data_sf$step_geometry
       ),
       projection = proj4,
-      class = c("sftraj", 'data.frame')
-      beginning = 
-        end = 
+      class = c("sfstep", 'data.frame')
     )
+
   }
