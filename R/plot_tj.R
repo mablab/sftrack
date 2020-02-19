4a9c6d41024528f67b7c5164d060b45a2a983974
birderboone <mttboone@gmail.com>
2020-02-19 16:59:30 -0500

    added plot functions as well as fixed how burst works and made it subsettable, and how it shows up using str.

---
+++ R/plot_tj.R
@@ -0,0 +1,20 @@
+plot.sftrack <- function(x,...){
+plot(x$geometry)
+burst_srt <- burst_sort(x$burst)
+
+new_clrs <- rainbow(length(unique(burst_srt)))
+  for(i in seq_along(new_clrs)){
+    #i=1
+    lvl <- levels(burst_srt)[i]
+  plot(x$geometry[burst_lbl==lvl], type='l',add = T ,col=new_clrs[i])
+  }
+}
+
+# plot(my_track)
+geom_sftraj <- function(data,...){
+  sub <- data[!sapply(data$geometry, function(x) all(is.na(x[[1]]))),]
+  geom_sf(data = sub, aes(color=burst_sort(sub$burst)))
+}
+#library(ggplot2)
+#ggplot(data=my_track) +geom_sf(aes(color=burst_sort(my_track$burst)))
+#ggplot() + geom_sftraj(my_step)
