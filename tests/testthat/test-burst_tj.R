aec4d0d4cd119a6f3433d2a1f5472b8672ffc1c9
birderboone <mttboone@gmail.com>
2020-03-29 18:48:32 -0600

    Changed how active bursts and sortIndex work. Now active_bursts only exist at the multiburst level and sort_index does all the heavy liftin on creating labels. Also tweaked the vignette

--- tests/testthat/test-burst_tj.R
+++ tests/testthat/test-burst_tj.R
@@ -6,15 +6,15 @@ test_that("Bursts build correctly", {
 
 
 })
-
-test_that("testing how burst defines levels",{
-
-  burstz <- list(id = rep(1,4),month = c(1,1,2,2))
-  # multi burst change in levels
-  df2 <- suppressMessages(make_multi_burst(burst_list=burstz,  burst_levels = list('id'=1:5,'month' = 0:11)))
-  expect_equal(attr(df2,'burst_levels')$month, paste0(0:11))
-  # multi burst no change
-  df3 <- suppressMessages(make_multi_burst(burst_list=burstz))
-  expect_equal(attr(df3,'burst_levels')$id, '1')
-  expect_equal(attr(df3,'burst_levels')$month, c('1','2'))
-})
+#
+# test_that("testing how burst defines levels",{
+#
+#   burstz <- list(id = rep(1,4),month = c(1,1,2,2))
+#   # multi burst change in levels
+#   df2 <- suppressMessages(make_multi_burst(burst_list=burstz,  burst_levels = list('id'=1:5,'month' = 0:11)))
+#   expect_equal(attr(df2,'burst_levels')$month, paste0(0:11))
+#   # multi burst no change
+#   df3 <- suppressMessages(make_multi_burst(burst_list=burstz))
+#   expect_equal(attr(df3,'burst_levels')$id, '1')
+#   expect_equal(attr(df3,'burst_levels')$month, c('1','2'))
+# })
