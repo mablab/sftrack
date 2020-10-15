657290cca7935d641aedce4f9b53adf17ba20622
birderboone <mttboone@gmail.com>
2020-10-15 15:19:52 -0600

    changing description file

--- R/init.R
+++ R/init.R
@@ -1,2 +1,3 @@
+#' @importFrom methods setClass
 setClass("sftrack", slots = c(time_col = "character", group_col = "character", sf_column = "character"))
 setClass("sftraj", slots = c(time_col = "character", group_col = "character", sf_column = "character"))
